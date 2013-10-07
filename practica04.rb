05d828e3415b88fc41cb09b80845a27619700967
ajyanreyu <ajyanreyu@localhost.localdomain>
2013-10-07 17:51:20 +0100

    creado el archivo inicial

---
+++ practica04.rb
@@ -0,0 +1,52 @@
+#! /usr/bin/ruby
+
+matrizA = []
+valor = 0
+for i in (0..4)
+	matrizA[i] = []
+	for j in (0..4)
+		matrizA[i][j] = valor
+		valor +=  1
+	end
+end
+
+matrizB = []
+valor = 0
+for i in (0..4)
+	matrizB[i] = []
+	for j in (0..4)
+		matrizB[i][j] = valor
+		valor += 1
+	end
+end
+
+for i in (0..4)
+	for j in (0..4)
+		print " ", matrizA[i][j], " "
+	end
+	print "\n"
+end
+
+print "\n"
+
+for i in (0..4)
+	for j in (0..4)
+		print " ", matrizB[i][j], " "
+	end
+	print "\n"
+end
+
+print "\n"
+
+	msuma = []
+	valor = 0
+	for i in (0..4)
+		msuma[i] = []
+		for j in (0..4)
+			msuma[i][j] = matrizA[i][j] + matrizB[i][j]
+			print msuma[i][j], " "
+		end
+		print "\n"
+	end
+
+print "\n"
